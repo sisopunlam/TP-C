cd960c104d84307cc26dda9e8f6bbc30f7071600
Meza Julian <julianmezare5@gmail.com>
2019-06-30 17:17:26 -0300

    Ejercicio 5 primera adaptacion

---
+++ Ejercicio5/cliente.c
@@ -0,0 +1,367 @@
+/*
+ Ejercicio 5 - Trabajo Práctico 3 - Primer entrega
+
+ Integrantes:
+
+# DNI       |APELLIDO      |NOMBRE
+
+ */
+
+include<sys / types.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
+
+#include <sys/ipc.h>   //biblioteca para los flags IPC
+#include <sys/shm.h>   //Para utilizar memoria compartida
+#include <fcntl.h>     //Para utilizar los flags 0
+#include <semaphore.h> //Para utilizar los semaforos POSIX
+
+    typedef struct consulta
+{
+    int patente;
+    char partido[41];
+    char titular[20];
+    float monto;
+} tConsulta;
+
+sem_t *consultar;
+sem_t *mensaje;
+sem_t *respuesta;
+
+char *msj;
+
+char peticion[100];
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void signalHandler(int);
+void signalHandler2(int);
+
+void enviar(const char *cad);
+void enterParaContinuar();
+void ayuda();
+
+int main(int argc, char *argv[])
+{
+    tConsulta msg;
+    int len;
+    char peticion[100];
+    char tecla;
+    int nTecla;
+    char menu_option[100];
+
+    if (argc != 2)
+    {
+        printf("Debe ingresar nombre del partido.\nVea la ayuda con ./cliente -h\n");
+        return 1;
+    }
+
+    if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0 || strcmp(argv[1], "-?") == 0)
+    {
+        //aca va la ayuda...
+        ayuda();
+        exit(1);
+    }
+
+    if (strlen(argv[1]) > 40)
+    {
+        printf("El nombre del partido debe ser de maximo 40 caracteres\n");
+        exit(1);
+    }
+
+    strcpy(msg.materia, argv[1]);
+
+    //Abre semaforos para comunicar procesos
+    consultar = sem_open("/consultar", 0);
+    mensaje = sem_open("/mensaje", 0);
+    respuesta = sem_open("/respuesta", 0);
+
+    if (mensaje == NULL || respuesta == NULL || consultar == NULL)
+    {
+        printf("Semaforos no estan creados.\nServidor cerrado.\n");
+
+        sem_close(consultar);
+        sem_close(mensaje);
+        sem_close(respuesta);
+        exit(0);
+    }
+
+    int shmid = shmget(234, 100 * sizeof(char), 0);
+
+    if (shmid == -1)
+    {
+        printf("La Memoria compartidad no se puede encontrar.\n");
+
+        sem_close(consultar);
+        sem_close(mensaje);
+        sem_close(respuesta);
+        exit(0);
+    }
+
+    msj = (char *)shmat(shmid, NULL, 0);
+
+    ////atrapar señales para cerrar los semaforos y desvincular la memoria compartida al terminar////
+
+    signal(SIGINT, signalHandler);
+    signal(SIGTERM, signalHandler);
+    signal(SIGQUIT, signalHandler);
+    signal(SIGUSR1, signalHandler2);
+
+    //Conectandose al servidor
+
+    sprintf(peticion, "n%d", getpid());
+    enviar(peticion);
+
+    do
+    {
+        //system("clear");
+        printf("\033c"); //limpia las pantallas, solo en sistemas unix
+        printf("--Menu Principal--\n");
+        printf("a. Cargar una multa.\n");
+        printf("b. Consultar multas por partido.\n");
+        printf("c. Consultar conductores suspendidos.\n");
+        printf("d. cancelar multas.\n");
+        printf("e. Salir.\n");
+        printf("\nIngrese una opcion: ");
+
+        nTecla = 0;
+        scanf(" %c", &menu_option[0]);
+
+        do
+        {
+            scanf("%c", &tecla);
+            nTecla++;
+        } while (tecla != '\n');
+
+        if (nTecla > 1)
+            menu_option[0] = 'i';
+
+        switch (menu_option[0])
+        {
+
+        case 'a':
+
+            //system("clear");
+            printf("\033c"); //limpia las pantallas, solo en sistemas unix
+            printf("--Menu cargar multas--\n");
+            printf("\nIngrese la patente del conductor: ");
+            scanf("%s", peticion);
+
+            if (strlen(peticion) > 9)
+            {
+                printf("\n--patente invalido, debe ser mayor de maximo 9 caracteres.--\nPresione Enter para continuar.");
+                enterParaContinuar();
+                break;
+            }
+
+            msg.patente = atoi(peticion);
+
+            if (msg.patente < 1)
+            {
+                printf("\n--PATENTE invalido, debe ser mayor a 0.--\nPresione Enter para continuar.");
+                enterParaContinuar();
+                break;
+            }
+
+            //system("clear");
+            printf("\033c"); //limpia las pantallas, solo en sistemas unix
+            printf("--Menu cargar multa--\n");
+            printf("PATENTE: %d\n", msg.patente);
+            printf("Ingrease nombre de titular.\n");
+            scanf("%s", peticion);
+
+            msg.titular = atof(peticion);
+            // len = atoi(peticion);
+            //strcpy(msg.titular, len);
+
+            //system("clear");
+            printf("\033c"); //limpia las pantallas, solo en sistemas unix
+            printf("--Menu cargar multa--\n");
+            printf("PATENTE: %d\n", msg.patente);
+            printf("Titular: %s\n", msg.titular);
+
+            printf("\nIngrese un monto: ");
+            scanf("%s", peticion);
+            msg.monto = atof(peticion);
+
+            if (msg.monto < 0)
+            {
+                printf("\n--Opcion invalida el montoa debe ser mayor a 0.--\nPresione Enter para continuar.");
+                enterParaContinuar();
+                break;
+            }
+            sprintf(peticion, "a%d,%s,%s,%.2f", msg.patente, msg.partido, msg.titular, msg.monto);
+            enviar(peticion);
+            printf("Presione Enter para continuar\n");
+            enterParaContinuar();
+            break;
+
+        case 'b': ///Aca deberia enviarle el patente ,partido, y opcion b.
+
+            //system("clear");
+            printf("\033c"); //limpia las pantallas, solo en sistemas unix
+            printf("--Menu promedio materia--\n");
+            printf("--Menu multas por partido--\n");
+            printf("\nIngrese ela patente del conductor: ");
+            scanf("%s", peticion);
+            msg.patente = atoi(peticion);
+
+            if (msg.patente < 1)
+            {
+                printf("\n--PATENTE invalido, debe ser mayor a 0.--\nPresione Enter para continuar.");
+                enterParaContinuar();
+                break;
+            }
+            sprintf(peticion, "b%d,%s", msg.patente, msg.partido);
+
+            enviar(peticion);
+            printf("Presione Enter para continuar\n");
+            enterParaContinuar();
+            break;
+
+        case 'c': //Aca deberia enviarle el DNI ,y opcion c.
+
+            //system("clear");
+            printf("\033c"); //limpia las pantallas, solo en sistemas unix
+            printf("--Menu SUSPENCIONES--\n");
+            printf("\nIngrese la  patente del conductor: ");
+            scanf("%s", peticion);
+            msg.patente = atoi(peticion);
+
+            if (msg.patente < 1)
+            {
+                printf("\n--PATENTE invalido, debe ser mayor a 0.--\nPresione Enter para continuar.");
+                enterParaContinuar();
+                break;
+            }
+
+            sprintf(peticion, "c%d", msg.patente);
+            enviar(peticion);
+            printf("Presione Enter para continuar\n");
+            enterParaContinuar();
+            break;
+        case 'd': //Aca deberia enviarle el DNI ,y opcion d.
+
+            //system("clear");
+            printf("\033c"); //limpia las pantallas, solo en sistemas unix
+            printf("--Menu cancelar multa--\n");
+            printf("\nIngrese la  patente del conductor: ");
+            scanf("%s", peticion);
+            msg.patente = atoi(peticion);
+
+            if (msg.patente < 1)
+            {
+                printf("\n--PATENTE invalido, debe ser mayor a 0.--\nPresione Enter para continuar.");
+                enterParaContinuar();
+                break;
+            }
+
+            sprintf(peticion, "d%d", msg.patente);
+            enviar(peticion);
+            printf("Presione Enter para continuar\n");
+            enterParaContinuar();
+            break;
+
+        case 'e':
+            signalHandler(1);
+            break;
+        default:
+            printf("\nOpcion invalida\n");
+            printf("Presione Enter para continuar\n");
+            enterParaContinuar();
+            break;
+        }
+
+    } while (menu_option[0] != 'e');
+
+    shmdt(&msj);
+    sem_close(consultar);
+    sem_close(mensaje);
+    sem_close(respuesta);
+    exit(0);
+}
+
+//funcion que se dispara al atrapar las señales que cierran el proceso
+void signalHandler(int dummy)
+{
+    sprintf(peticion, "e%d", getpid());
+    enviar(peticion);
+    printf("programa terminado.\n");
+    shmdt(&msj);
+    sem_close(consultar);
+    sem_close(mensaje);
+    sem_close(respuesta);
+    exit(0);
+}
+
+//funcion que se dispara al recibir la señal SIGUSR1 del servidor
+void signalHandler2(int dummy)
+{
+    printf("Servidor cerrado.\n");
+    printf("programa terminado.\n");
+    shmdt(&msj);
+    sem_close(consultar);
+    sem_close(mensaje);
+    sem_close(respuesta);
+    exit(0);
+}
+
+void enterParaContinuar()
+{
+    char tecla = 'b';
+    while (tecla != '\n')
+    {
+        scanf("%c", &tecla);
+    }
+    scanf("%c", &tecla);
+    while (tecla != '\n')
+    {
+        scanf("%c", &tecla);
+    }
+}
+
+void enviar(const char *cad)
+{
+    pthread_mutex_lock(&mutex); //semaforo bloqueado para que cada send del main al servidor espere que la respuesta la reciba el siguiente thread
+
+    sem_wait(consultar); //Pide la Memoria compartida para que ningun otro cliente pueda usarla
+    strcpy(msj, cad);    //Escribe en Memoria compartida
+    sem_post(mensaje); //Avisa al servidor que hay un mensaje
+    printf("Mensaje enviado, esperando respuesta...\n");
+    sem_wait(respuesta); //Espera respuesta del servidor
+    printf("\033c");                //limpia las pantallas, solo en sistemas unix
+    printf("Respuesta: %s\n", msj); //Imprime respuesta
+
+    if (msj[0] == '-')
+    {
+        shmdt(&msj);
+        sem_post(consultar); //Libera la memoria para otro cliente
+        sem_close(consultar);
+        sem_close(mensaje);
+        sem_close(respuesta);
+        printf("Programa terminado.\n");
+        exit(0);
+    }
+
+    sem_post(consultar); //Libera la memoria para otro cliente
+    pthread_mutex_unlock(&mutex);
+}
+
+void ayuda()
+{
+    printf("---------------\ncliente -h\ncliente -help\ncliente -? :\n\t\t muestran esta ayuda\n\n");
+    printf("---------------\ncliente 'nombre_del partido'\n\n");
+    printf("\t\tEn esta forma el programa se conecta al servidor y se identifica al profesor con el nombre del partido indicada.\n");
+
+    printf("---------------\n\t\tAl iniciar el programa se muestra un menu en el cual el usuario puede agregar un nuevo registro en la base de datos, ");
+    printf("consultar el promedio de un infractor de  su partido, o consultar las multas de todos las multas de todos los partidos.\n");
+    printf("\t\tLas patentes ingresados deben ser mayor a 0, de maximo 9 caracteres, el nombre de la materia de minimo 1 caracter y de maximo 40 caracteres, ");
+    printf("las multas ingresadas deben ser mayor a 0 en caso de ser necesario el separador decimal es el punto.\n");
+    printf("\nEjemplo de ejecucuion: ./cliente sisop\n");
+    printf("\nArchivo de prueba en multas.txt contiene las patentes 1111-2222-3333\n");
+}
+
---
+++ Ejercicio5/servidor.c
@@ -0,0 +1,323 @@
+/*
+ Ejercicio 4 - Trabajo Práctico 3
+
+ Integrantes:
+
+# DNI       |APELLIDO      |NOMBRE
+#
+
+# 39463982   Meza          Julian
+
+ */
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
+#include "lista.h"
+
+#include <sys/ipc.h> //biblioteca para los flags IPC
+#include <sys/shm.h> //Para utilizar memoria compartida
+#include <sys/stat.h>
+#include <fcntl.h>	 //Para utilizar los flags 0
+#include <semaphore.h> //Para utilizar los semaforos POSIX
+
+sem_t *consultar;
+sem_t *mensaje;
+sem_t *respuesta;
+int shmid;
+
+char *archivo = "multas.txt";
+
+int clients[100];
+int conectados = 0;
+int cant_client;
+
+tLista lista;
+char solicitud[100];
+char *msj;
+char *car;
+
+void signalHandler(int);
+
+void procesarPeticion(const char *peticion, char *responder);
+
+void ayuda();
+
+int main(int argc, char *argv[])
+{
+
+	if (argc < 2 || argc > 3)
+	{
+		printf("Debe ingresar maxima cantidad de clientes, y archivo de multas (por defecto 'multas.txt').\nVea la ayuda con ./servidor -h\n");
+		return 1;
+	}
+
+	//AYUDA
+	if (argc == 2)
+	{
+		if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0 || strcmp(argv[1], "-?") == 0)
+		{
+			ayuda();
+			exit(1);
+		}
+	}
+
+	cant_client = atoi(argv[1]);
+
+	if (cant_client < 1 || cant_client > 100)
+	{
+		printf("Primer parametro debe ser la cantidad maxima de clientes que debe ser un numero entero entre 1 y 100.\nVea la ayuda con ./servidor -h\n");
+		exit(1);
+	}
+
+	if (argc == 3)
+	{
+		archivo = argv[3];
+	}
+
+	crearLista(&lista);
+
+	if (cargarListaConArchivo(&lista, archivo) != 0)
+	{
+		printf("Error al cargar el archivo de multas\n");
+		vaciarLista(&lista);
+		return 1;
+	}
+
+	//Abre semaforos para comunicar procesos
+	consultar = sem_open("/consultar", O_CREAT | O_EXCL, 0666, 0);
+	mensaje = sem_open("/mensaje", O_CREAT | O_EXCL, 0666, 0);
+	respuesta = sem_open("/respuesta", O_CREAT | O_EXCL, 0666, 0);
+	if (mensaje == NULL || respuesta == NULL || consultar == NULL)
+	{
+		printf("No se pueden crear semaforos, puede que los este usando otro proceso servidor.\nServidor cerrado.\n");
+		vaciarLista(&lista);
+		//Cierre de semaforos
+		sem_close(consultar);
+		sem_close(mensaje);
+		sem_close(respuesta);
+		exit(0);
+	}
+
+	shmid = shmget(234, 100 * sizeof(char), IPC_CREAT | IPC_EXCL | 0666);
+	//shmid = shmget(234,100 * sizeof(char), IPC_CREAT | 0666 );
+
+	if (shmid == -1)
+	{
+		printf("La Memoria compartida no se puede obtener, puede que la este usando otro proceso servidor.\n");
+		vaciarLista(&lista);
+		//Cierre de semaforos
+		sem_close(consultar);
+		sem_close(mensaje);
+		sem_close(respuesta);
+		//Marcar semaforo para destruirlo
+		sem_unlink("/consultar");
+		sem_unlink("/mensaje");
+		sem_unlink("/respuesta");
+
+		//shmctl(shmid,IPC_RMID,NULL);	//Marcar la memoria compartida para borrar
+		exit(0);
+	}
+
+	msj = (char *)shmat(shmid, NULL, 0);
+
+	////atrapar señales para cerrar los sockets al terminar////
+
+	signal(SIGINT, signalHandler);
+	signal(SIGTERM, signalHandler);
+	signal(SIGQUIT, signalHandler);
+
+	printf("Memoria y semaforos creados\n");
+	printf("Esperando al clientes\n");
+
+	sem_post(consultar);
+
+	while (1)
+	{
+		sem_wait(mensaje);		//Espera un mensaje
+		strcpy(solicitud, msj); //Escribe en Memoria compartida
+		procesarPeticion(solicitud, msj);
+		sem_post(respuesta); //Avisa al servidor que hay un mensaje
+	}
+
+	return 0;
+}
+
+void signalHandler(int dummy) //funcion que se dispara al atrapar las señales que cierran el proceso
+{
+	sem_wait(consultar);
+
+	for (int i = 0; i < conectados; i++) //Informa a los clientes que el servidor se cierra
+	{
+		kill(clients[i], SIGUSR1);
+	}
+
+	shmdt(&msj);				   //Desvincular la memoria compartida de la variable local
+	shmctl(shmid, IPC_RMID, NULL); //Marcar la memoria compartida para borrar
+
+	//Cierre de semaforos
+	sem_close(consultar);
+	sem_close(mensaje);
+	sem_close(respuesta);
+
+	//Marcar semaforo para destruirlo
+	sem_unlink("/consultar");
+	sem_unlink("/mensaje");
+	sem_unlink("/respuesta");
+
+	vaciarLista(&lista);
+
+	printf("Servidor cerrado.\n");
+
+	exit(0);
+}
+
+void procesarPeticion(const char *peticion, char *responder)
+{
+	int ini;
+	int fin;
+	int i;
+	int j;
+	float promedio;
+	unsigned long patente;
+	int pid_cliente;
+	char *partido;
+	char aux[100];
+
+	switch (peticion[0])
+	{
+
+	case 'n':
+		pid_cliente = atoi(&peticion[1]);
+		//Se verifica si quedan lugares libres en la sala..
+		if (conectados < cant_client)
+		{
+			clients[conectados] = pid_cliente;
+			printf("Se ha conectado un nuevo usuario\nPid: %d\n", pid_cliente);
+			conectados++;
+			strcpy(responder, "Conectado al servidor.");
+		}
+		else
+		{
+			printf("Cliente rechazado, demasiados clientes conectados actualmente\n");
+			strcpy(responder, "--Intento de conexion fallida: se ha alcanzado el maximo de conexiones posibles\n");
+		}
+		break;
+	case 'a':
+		if ((cargarRegistroEnListaYArchivo(&lista, archivo, &peticion[1])) == 1)
+		{
+			strcpy(responder, "Registro no se pudo agregar");
+		}
+		else
+		{
+			strcpy(responder, "Registro agregado");
+		}
+		break;
+	case 'b':
+		///patente////
+		ini = 1;
+		fin = 1;
+		while (peticion[fin] != ',')
+		{
+			fin++;
+		}
+
+		strncpy(aux, &peticion[ini], fin);
+		aux[fin - ini] = '\0';
+
+		patente = strtoul(aux, &car, 10);
+		//////////////////////////////////////////
+		///partido////
+		fin++;
+
+		strcpy(aux, &peticion[fin]);
+		promedio = promedioPartido(&lista, patente, aux); //esta fuincio de be ser cambiada por
+
+		if (promedio == -1)
+		{
+			sprintf(responder, "No hay multas registrados de %lu para %s.\n", patente, aux);
+		}
+		else
+		{
+			sprintf(responder, "Monto total de multas de %s de %lu: %.2f\n", aux, patente, promedio);
+		}
+
+		////////////////////////////////////
+		break;
+	case 'c':
+		///patente////
+		patente = strtoul(&peticion[1], &car, 10);
+
+		promedio = promedioGeneral(&lista, patente);
+
+		if (promedio == -1)
+		{
+			sprintf(responder, "No hay multas registrados de %lu.\n", patente);
+		}
+		else
+		{
+			sprintf(responder, "Registro a SUSPENDER\nMonto total de multas de %lu: %.2f\n", patente, promedio);
+		}
+
+	case 'd':
+		///patente////
+		patente = strtoul(&peticion[1], &car, 10);
+		promedio = cancelarDeuda(&lista, patente);
+
+		if (promedio == -1)
+		{
+			sprintf(responder, "No hay multas registrados de %lu.\n", patente);
+		}
+		else
+		{
+			sprintf(responder, "Sea han cancelado todas las multas de %lu\n", patente);
+		}
+
+		break;
+	case 'e':
+		///Salio un cliente////
+		pid_cliente = atoi(&peticion[1]);
+		printf("Se desconecto un usuario de la sala. Pid: %d\n", pid_cliente);
+		strcpy(responder, "--Servidor saco al cliente correctamente.\n");
+		for (i = 0; i < conectados; i++)
+		{
+			if (clients[i] == pid_cliente)
+			{
+				j = i;
+				while (j < conectados - 1)
+				{
+					clients[j] = clients[j + 1];
+					clients[j] = clients[j + 1];
+					j++;
+				}
+			}
+		}
+		conectados--;
+
+		////////////////////////////////////
+
+		break;
+	default:
+		strcpy(responder, "opcion invalida");
+		break;
+	}
+}
+
+void ayuda()
+{
+	printf("---------------\n\t\tEste programa es un servidor que responde a las solicitudes que hacen profesores, a travez de otro programa cliente, ");
+	printf("para agregar notas, o cunsultar promedios de alumnos, cuyas notas y parciales estan en un archivo de texto.\n");
+	printf("\n----Formas de usar el programa----\n");
+	printf("---------------\nservidor -h\nservidor -help\nservidor -? :\n\t\t muestran esta ayuda\n\n");
+	printf("---------------\nservidor 'max_cant_clientes' 'nombre_de_archivo_de_notas'\nservidor 'max_cant_clientes'\n\n");
+	printf("\t\tAl iniciar el programa el servidor establece un maximo de clientes indicado en el primer parametro que debe ser un valor entero entre 1 y 100.\n");
+	printf("Por defecto el archivo con las notas es 'notas.txt', a menos que se le indique otro archivo con el segundo parametro se le indique otra cosa.\n");
+	printf("Si el archivo de texto no existe se crea, y si existe se cargan los datos y agregan al final los nuevos registros.\n");
+	printf("El archivo de notas debe estar en el directorio del programa para que lo reconozca\n");
+	printf("Solo puede correr un proceso servidor en el sistema, para que los semaforos y memoria compartida no se vean modificados incorrectamente.\n");
+	printf("\nEjemplo de ejecucuion: ./servidor 10\n");
+}
